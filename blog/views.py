c6188b3250c84e5f426840ad8af0dfd3707a48dd
Eri Schon <erischon@gmail.com>
2021-01-21 13:51:14 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ def all_blogs(request):
     # blogs = Blog.objects.all()
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
